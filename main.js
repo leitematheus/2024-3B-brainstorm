d9bff2102a976b0c3e1f95f17cd5d6f83e600a4f
Matheus Henrique Leite da Silva <110172122+leitematheus@users.noreply.github.com>
2024-06-20 08:29:26 -0300

    20/06 boraa

---
+++ main.js
@@ -0,0 +1,42 @@
+const caixaPrincipal = document.querySelectorAll('.caixa-principal')
+const caixaPerguntas = document.querySelectorAll('.caixa-perguntas')
+const caixaAlternativas = document.querySelectorAll('.caixa-alternativas')
+const caixaResultado = document.querySelectorAll('.caixa-resultado')
+const textoResultado = document.querySelectorAll('.texto-resultado')
+const perguntas = [
+    {
+        enunciado: 'Paulo tem 17 anos de idade, e pensando sobre qual carreira seguir, ele para pensar o que ele faz de melhor e gosta de fazer.<br>Nisso lhe vem duas opcões em sua cabeça:',
+        alternativas: [
+            'Ele escolhe fazer ciência da computação, e ganhar razoavelmente, mas que futuramente pode melhora.',
+            'Ele escolhe um emprego que ganha muito mais que ciência da computação, mas não gosta da área.'
+        ]
+    },
+    {
+        enunciado: 'Pergunta2',
+        alternativas: [
+            'alternativa 1',
+            'alternativa 2'
+        ]
+    },
+    {
+        enunciado: 'Pergunta3',
+        alternativas: [
+            'alternativa 1',
+            'alternativa 2'
+        ]
+    },
+    {
+        enunciado: 'Pergunta4',
+        alternativas: [
+            'alternativa 1',
+            'alternativa 2'
+        ]
+    },
+    {
+        enunciado: 'Pergunta5',
+        alternativas: [
+            'alternativa 1',
+            'alternativa 2'
+        ]
+    }
+]
