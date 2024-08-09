ddcd44b54b2ee220cbb735f4c2552fb584e38867
Matheus Henrique Leite da Silva <110172122+leitematheus@users.noreply.github.com>
2024-08-09 15:12:05 -0300

    09/08 feito

--- main.js
+++ main.js
@@ -5,50 +5,40 @@ const caixaResultado = document.querySelector('.caixa-resultado')
 const textoResultado = document.querySelector('.texto-resultado')
 const perguntas = [
     {
-        enunciado: 'Nome, mora em Foz do Iguaçu, que é mundialmente conhecido pelas Cataratas, e nome sabe a importância de preservar a água ainda mais em sua cidade, por ser um lugar turístico relacionado a água, nisso nome decide:',
+        enunciado: 'Nome tem 20 anos e mora em Foz do Iguaçu, que é mundialmente conhecido pelas Cataratas, e nome sabe a importância de preservar a água ainda mais em sua cidade, por ser um lugar turístico relacionado a água, nisso nome decide:',
         alternativas: [
-         {
-            texto: 'Nome escolhe estudar para futuramente trabalhar na Itaipu, e preservar a água e fornecer para o seu país e o país vizinho!',
-            afirmação: 'Nome pensa que não é quaificado(a) para este trabalho, e acha melhor deixar na mão de outra pessoa o seu futuro e o da água.'
-         },
-         {
-            texto: 'Nome começa a trabalhar na Itaipu, e lá surge uma oportunidade exelente para cuidar da qualidade da água do lago da Itaipu, nome aproveita essa oportunidade?',
-            afirmacao: 'Nome vira mendingo e começa na vida do crime.'
-         }
+            {
+                afirmacao: 'Nome escolhe estudar para futuramente trabalhar na Itaipu, e preservar a água e fornecer para o seu país e o país vizinho!',
+                texto: 'Nome sempre gostou da Itaipu e sempre se importou com todos ao seu redor.'
+            },
         ]
     },
     {
-        enunciado: 'Nome começa a trabalhar na Itaipu, e lá surge uma oportunidade exelente para cuidar da qualidade da água do lago da Itaipu, nome aproveita essa oportunidade?',
+        enunciado: 'Nome com 22 anos começa a cuidar do lago da Itaipu e lá descobre um novo tipo de bactéria presnte na água, o que nome deve fazer?',
         alternativas: [
-        {
-            texto: 'Sim, pois esse é o seu sonho e você se ver responsável em preservar a água da sua cidade.',
-            afirmacao:'Não quer esse cargo, pois o seu salário já está ótimo assim e não quer saber mais de preservar a água, porque a sua condição financeira subiu à cabeça.'
-        }
+            {
+                afirmacao: 'Nome avisa aos supervisores, e começa a trabalhar como chefe de outros cientistas, contra essa nova bactéria.',
+                texto: 'Nome quer salvar a todos.'
+            },
+            {
+                afirmacao: 'Nome anula as bactérias sozinho.',
+                texto: 'Nome salva a todos sozinho.'
+            }
         ]
     },
     {
-        enunciado: 'Nome começa a cuidar do lago da Itaipu e lá descobre um novo tipo de bactéria presnte na água, o que nome deve fazer?',
+        enunciado: 'Nome anula todas as bactérias, e acaba sendo promovido tendo uma ótima carreira.',
         alternativas: [
-    {
-           texto: 'Nome decide não avisar, pois pensa que deve ser uma bactéria inofensiva e não causará nenhum mal',
-            afirmacao: 'Nome avisa aos supervisores, e começa a trabalhar como chefe de outros cientistas, contra essa nova bactéria.'
-    }
+            {
+                afirmacao: 'Nome decedi se aposentar aos 40, pois já têm muitos feitos em sua carreira profissional e quer deixar o jovens terem novas oportunidades.',
+                texto: 'Nome está feliz e em paz consigo mesmo.'
+            },
+            {
+                afirmacao: 'Nome se aposenta aos 60 e vive tranqulo, mas sem forças para aproveitar o resto da vida.',
+                texto: 'Nome continua na carreira'
+            }
         ]
     },
-    {
-        enunciado: 'Nome perde o emprego e fica desempregado, pois a bactéria se propagou, e levou 10% da cidade à óbito e terá que cumprir uma pena.',
-        alternativas: [
-            'Nome decide se render, e cumprir a pena pela sua negligência, para pelo menos fazer algo certo na vida.',
-            'Nome decide contratar um advogado e escapar da sua condenação, e viver uma vida em paz'
-        ]
-    },
-    {
-        enunciado: 'Nome anula todas as bactéria salvando a sociedade, e ganha a chave da cidade!',
-        alternativas: [
-            'Nome decedi se aposentar aos 40, pois já têm muitos feitos em sua carreira profissional e quer deixar o jovens à terem oportunidades.',
-            'Nome decide entrar mais afundo na sua profissão, e depois de 20 anos se aposentar.'
-        ]
-    }
 ]
 
 let atual = 0;
@@ -86,7 +76,7 @@ function respostaSelecionada(opcaoSelecionada){
 function mostreResultado(){
     caixaPerguntas.textContent = " Em 2049...";
     textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent = " ";
+    caixaAlternativas.textContent = "";
 }
 mostraPergunta();
 
